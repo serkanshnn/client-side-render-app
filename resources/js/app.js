f0ad7ef68038a524d0de6c63991ee0dc7aa516f9
Serkan Şahin <serkan.sahin@epigra.com>
2021-04-06 02:05:57 +0300

    Add vue, moment and form validation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-require('./bootstrap');
+import {createApp, h} from "vue";
+import Users from "./components/Users/Users"
+import Layout from "./components/Layout";
+import User from "./components/Users/User";
+import CreateUser from "./components/Users/CreateUser";
+
+const root = document.getElementById('app')
+const app = createApp({
+    render(){
+        console.log(JSON.parse(root.dataset.props));
+        return h(app.component(root.dataset.component), JSON.parse(root.dataset.props));
+    }
+});
+app.component('Layout', Layout)
+app.component('Users', Users)
+app.component('User', User)
+app.component('CreateUser', CreateUser)
+app.mount('#app');
